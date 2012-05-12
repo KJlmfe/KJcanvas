9e63bdb67ef1f09923741cff5e1e9ef8aa88ab65
KJlmfe <kjlmfe@gmail.com>
2012-05-12 17:31:46 +0800

    添加canvas对象,修复多个对象一起移动时的bug(移动路线一样),添加了详细注释,完善矩形向各个方向移动的情况(目前多个图像移动时,属于并发移动,需增加顺序移动模式)

--- Animation/rectangle.js
+++ Animation/rectangle.js
@@ -1,18 +1,44 @@
-var Shape = new Array();
-function canvasSave(obj)
+canvas = function(width,height)
 {
-	Shape.push(obj);
+	this.w = width;
+	this.h = height;
+	this.Shape = new Array(); //用于保存画板上存在的图形对象
 }
-function canvasRestore()
+canvas.prototype.exist = function(obj)  //判断obj图形对象是否在画板上存在
 {
-	for(i=0;i<Shape.length;i++)
-		Shape[i].draw(1);
+	for(i=0;i<this.Shape.length;i++)
+		if(this.Shape[i] == obj)
+			return true;
+	return false;
 }
-function Rectangle(ctx,width,height,x,y,color) //矩形的宽和高
+canvas.prototype.save = function(obj) //保存obj图形对象到Shape数组
+{
+	this.Shape.push(obj);
+}
+canvas.prototype.restore = function() //将Shape里的所有图形对象重绘
+{
+	for(i=0;i<this.Shape.length;i++)
+		if(this.Shape[i])
+			this.Shape[i].draw();
+}
+canvas.prototype.del = function(obj) //从Shape里删除一个图形对象
+{
+	for(i=0;i<this.Shape.length;i++)
+		if(this.Shape[i] == obj)
+		{
+			this.Shape[i] = null;
+			break;
+		}
+}
+canvas.prototype.clear = function() //清空画板
+{
+	ctx.clearRect(0,0,this.w,this.h);
+}
+
+Rectangle = function(width,height,x,y,color) //矩形长、宽，位置，颜色
 {
 	this.id = this;
 	this.color = color;
-    this.ctx = ctx;
 	this.w = width;
 	this.h = height;
 	this.x = x;
@@ -20,13 +46,15 @@ function Rectangle(ctx,width,height,x,y,color) //矩形的宽和高
 }
 Rectangle.prototype.draw = function(x,y) //矩形左上角所在位置
 {
-	if(x && y)
+	if(!canvas.exist(this)) //每在画板上画一个图形对象，都要将该对象保存到画板的Shape里 
+		canvas.save(this);
+	if(x && y)   //更新当前位置
 	{
 		this.x = x;
 		this.y = y;
 	}
-	this.ctx.fillStyle = this.color;
-	this.ctx.fillRect(this.x,this.y,this.w,this.h);  //画一个实体矩形
+	ctx.fillStyle = this.color;
+	ctx.fillRect(this.x,this.y,this.w,this.h);  //画一个实体矩形
 //	this.ctx.strokeRect(this.x,this.y,this.w,this.h); //勾画出矩形边框
 //	this.ctx.clearRect(this.x,this.y,this.w,this.h);  //清空内部，只留边框	
 }
@@ -34,48 +62,106 @@ Rectangle.prototype.clear = function() //橡皮擦擦掉矩形
 {
 	this.ctx.clearRect(this.x,this.y,this.w,this.h);
 }
-Rectangle.prototype.move = function(x,y) //移动一个巨型
+Rectangle.prototype.move = function(x,y,speed,action) //移动一个巨型
 {
-	//清楚canvas
-	//把之前的每个对象都重画
-	//画当前正在移动的图像的下一个位置
-	//如果位置到了，则保存该图像到存在画面上的链表
-	//
-
-	var me = this;  //setInterval 里不能直接调用this.draw,所以使用变量作用域解决这个问题
-	//默认沿着两点间的直线路径移动
-	if(me.x != x)
+	if(x && y)  //设置目标位置
 	{
-	
-		k = (me.y - y) / (me.x - x);  
-		b = me.y - k*me.x;
-		IDD = setInterval(function(){
-			//擦干净画布
-			me.ctx.clearRect(0,0,canvas.width,canvas.height);
-			//把要移动的图形对象从画布上删除
-
-			//重画画布上的图形
-			canvasRestore();	
-			//绘制移动图形
-			me.x++;
-			y = k*me.x + b;
-			me.draw(me.x,y);
-			//判断是否到达目标位置
-			if(x == me.x)
-				clearInterval(IDD);
-		},24);
-
+		this.aimX = x;
+		this.aimY = y;
 	}
 	else
-	{	
-		x = me.x;
-		IDD = setInterval(function(){
-			me.clear();
-		//	me.ctx.save();
-			me.draw(x,++me.y);
-			if(y == me.y)
-				clearInterval(IDD);
-		//	me.ctx.restore();	
-		},22);
+		return;
+	if(speed)  //设置移动速度
+		this.speed = speed;
+	else
+		this.speed = 24;
+	if(action)  //设置移动路线
+		this.action = action;
+	else
+		this.action = "line";
+
+	var me = this;  //setInterval 里不能直接调用this.draw,所以使用变量作用域解决这个问题
+	
+	
+	//默认沿着两点间的直线路径移动
+	if(me.x != me.aimX)
+	{
+		me.k = (me.y - me.aimY) / (me.x - me.aimX);  
+		me.b = me.y - me.k*me.x;
+	}
+	else
+	{
+		me.k = 0;
+		me.b = 0;
+	}
+	if(this.aimX > this.x)   // 原图形右侧运动
+	{
+		this.timer = setInterval(function(){
+			//擦干净画布
+			canvas.clear();
+			//把要移动的图形对象从画布上删除
+			canvas.del(me);
+			//重画画布上的图形
+			canvas.restore();
+			//绘制移动图形
+			me.x++;
+			me.y = me.k*me.x + me.b;
+			me.draw(me.x,me.y);
+			//判断是否到达目标位置
+			if(me.aimX == me.x)
+				clearInterval(me.timer);
+		},me.speed);
+	}
+	else if(this.aimX < this.x)   // 原图形左侧运动
+	{
+		this.timer = setInterval(function(){
+			//擦干净画布
+			canvas.clear();
+			//把要移动的图形对象从画布上删除
+			canvas.del(me);
+			//重画画布上的图形
+			canvas.restore();
+			//绘制移动图形
+			me.x--;
+			me.y = me.k*me.x + me.b;
+			me.draw(me.x,me.y);
+			//判断是否到达目标位置
+			if(me.aimX == me.x)
+				clearInterval(me.timer);
+		},me.speed);
+	}
+	else if(this.aimY < this.y)   // 原图形正上方运动
+	{
+		this.timer = setInterval(function(){
+			//擦干净画布
+			canvas.clear();
+			//把要移动的图形对象从画布上删除
+			canvas.del(me);
+			//重画画布上的图形
+			canvas.restore();
+			//绘制移动图形
+			me.y--;
+			me.draw(me.x,me.y);
+			//判断是否到达目标位置
+			if(me.aimY == me.y)
+				clearInterval(me.timer);
+		},me.speed);
+	}
+	else if(this.aimY > this.y)   // 原图形正下方运动
+	{
+		this.timer = setInterval(function(){
+			//擦干净画布
+			canvas.clear();
+			//把要移动的图形对象从画布上删除
+			canvas.del(me);
+			//重画画布上的图形
+			canvas.restore();
+			//绘制移动图形
+			me.y++;
+			me.draw(me.x,me.y);
+			//判断是否到达目标位置
+			if(me.aimY == me.y)
+				clearInterval(me.timer);
+		},me.speed);
 	}
 }
