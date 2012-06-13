7fe1d614309e60736eb0cb5bf7074e4f8d9b2ba1
KJlmfe <kjlmfe@gmail.com>
2012-06-13 22:14:57 +0800

    初步完成BST动画演示

--- Algorithm/Binary Search Tree.js
+++ Algorithm/Binary Search Tree.js
@@ -17,14 +17,14 @@ BST.EMPTY_INFO = "堆栈里空空如也了,弹不出东西了！";
 BST.DATASHAPE_WIDTH = 30;     //宽度
 BST.DATASHAPE_HEIGHT = 30;   //长度
 
-BST.DATASHAPE_ROOT_X = 400;  //表头(栈顶)的位置
+BST.DATASHAPE_ROOT_X = 500;  //表头(栈顶)的位置
 BST.DATASHAPE_ROOT_Y = 20;
-BST.DATASHAPE_GAP_Y = 40;  //彼此之间的横向间隔
+BST.DATASHAPE_GAP_Y = 50;  //彼此之间的横向间隔
 BST.DATASHAPE_INSERT_X = 0;  //入栈元素的位置
 BST.DATASHAPE_INSERT_Y = 0;
 BST.DATASHAPE_GAP_CHILD_X = 200;
 BST.DATASHAPE_GAP_CHILD_Y = 70;
-BST.DATASHAPE_GAP_K = 50;
+BST.DATASHAPE_GAP_K = 20;
 
 BST.prototype = new Algorithm();
 
@@ -56,66 +56,39 @@ BST.prototype.node = function(value)  //堆栈节点
 		EndShape : this.DataShape
 	});
 }
-BST.prototype.create = function()  //初始化堆栈,并绘制该堆栈
-{
-	
-	Canvas.init();        //初始化一个洁白的画板
-	Canvas.cmd("Setup");  //开始动画动作
-	Canvas.cmd
-	(
-		"FadeIn", this.root.DataShape,  //表头淡入
-		{
-			x : BST.DATASHAPE_TOP_X,
-			y : BST.DATASHAPE_TOP_Y
-		}
-	);
-	Canvas.cmd("Other",function()
-	{
-		$(".controler").removeAttr("disabled");		//启用所有控制元素
-	});
-	Canvas.cmd("End");  //结束动画
-}
 BST.prototype.insert = function( value )
 {
 	this.disableControlBar();
 
 	Canvas.cmd("Setup");
 	var insertNode = new this.node( value );
-	Canvas.cmd
-	(
-		"Draw", insertNode.DataShape,
-		{
-			x : BST.DATASHAPE_INSERT_X,
-			y : BST.DATASHAPE_INSERT_Y,
-		}
-	);
-	if(this.root == null)
+	Canvas.cmd("Draw", insertNode.DataShape, {
+		x : BST.DATASHAPE_INSERT_X,
+		y : BST.DATASHAPE_INSERT_Y,
+	});
+	if(this.root == null)    //BST不存在，插入的点作为根节点
 	{
-		Canvas.cmd
-		(
-			"Move", insertNode.DataShape,
-			{
-				aim_x : BST.DATASHAPE_ROOT_X,
-				aim_y : BST.DATASHAPE_ROOT_Y,
-			}
-		);
+		Canvas.cmd("Move", insertNode.DataShape, {
+			aim_x : BST.DATASHAPE_ROOT_X,
+			aim_y : BST.DATASHAPE_ROOT_Y,
+		});
 		this.root = insertNode;
 	}
-	else
+	else		//插入的是非根节点
 	{
-		Canvas.cmd
-		(
-			"Move", insertNode.DataShape,
-			{
-				aim_x : this.root.DataShape.x,
-				aim_y : this.root.DataShape.y + BST.DATASHAPE_GAP_Y,
-			}
-		);
+		Canvas.cmd("Move", insertNode.DataShape, {
+			aim_x : this.root.DataShape.x,
+			aim_y : this.root.DataShape.y + BST.DATASHAPE_GAP_Y,
+		});
 		var tmp = this.root;
-		var level = 0;
+		var level = 1;
 		while(1)
 		{
 			Canvas.cmd("Delay");
+			Canvas.cmd("Move", insertNode.DataShape, {
+				aim_x : tmp.DataShape.x,
+				aim_y : tmp.DataShape.y + BST.DATASHAPE_GAP_Y,
+			});		
 			if(value < tmp.value)
 				if(tmp.left == null)
 				{
@@ -128,6 +101,8 @@ BST.prototype.insert = function( value )
 						},
 						"Move", tmp.LeftPointerShape,
 						{
+							alpha : 1,
+							EndShape : tmp.DataShape,
 							aimEnd_x : tmp.DataShape.x - (BST.DATASHAPE_GAP_CHILD_X - level * BST.DATASHAPE_GAP_K),
 							aimEnd_y : tmp.DataShape.y + BST.DATASHAPE_GAP_CHILD_Y,
 						}
@@ -136,18 +111,8 @@ BST.prototype.insert = function( value )
 					break;
 				}
 				else
-				{
 					tmp = tmp.left;
-					Canvas.cmd
-					(
-						"Move", insertNode.DataShape,   
-						{
-							aim_x : tmp.DataShape.x,
-							aim_y : tmp.DataShape.y + BST.DATASHAPE_GAP_Y,
-						}
-					);		
-				}
-			else
+			else if(value > tmp.value)
 				if(tmp.right == null)
 				{
 					Canvas.cmd
@@ -167,22 +132,19 @@ BST.prototype.insert = function( value )
 					break;
 				}
 				else
-				{
 					tmp = tmp.right;
-					Canvas.cmd
-					(
-						"Move", insertNode.DataShape,  
-						{
-							aim_x : tmp.DataShape.x,
-							aim_y : tmp.DataShape.y + BST.DATASHAPE_GAP_Y,
-						}
-					);		
-				}
+			else
+			{
+				Canvas.cmd("Other", function(){
+					alert(value + " 已经在BST里存在, 插入失败!");
+				});
+				Canvas.cmd("FadeOut", insertNode.DataShape);
+				break;
+			}
 			level++;
 		}
 	}
-	Canvas.cmd("Other",function()
-	{
+	Canvas.cmd("Other",function(){
 		$(".controler").removeAttr("disabled");		//启用所有控制元素
 	});
 	Canvas.cmd("End");
@@ -190,183 +152,146 @@ BST.prototype.insert = function( value )
 BST.prototype.delete = function( value )
 {
 	this.disableControlBar();
-
+	
 	Canvas.cmd("Setup");
-	var deleteNode = new this.node( value );
-	Canvas.cmd
-	(
-		"Draw", deleteNode.DataShape,
-		{
-			x : BST.DATASHAPE_INSERT_X,
-			y : BST.DATASHAPE_INSERT_Y,
-		}
-	);
-	var tmp = this.root;
-	var mother = this.root;
-	while(1)
+	var deleteValueShape = new Label({Canvas : Canvas, text : value});
+	Canvas.cmd("Draw", deleteValueShape, {
+		x : BST.DATASHAPE_INSERT_X,
+		y : BST.DATASHAPE_INSERT_Y,
+	});
+
+	var tmpNode = this.root;
+	var tmpMotherNode = null;
+	var deleteMotherNode = null, deleteNode = null, swapNode = null;
+
+	while(tmpNode != null)   //查找要删除的节点
 	{
 		Canvas.cmd("Delay");
-		if(tmp == null)			//没有找到
+		Canvas.cmd("Move", deleteValueShape, {
+			aim_x : tmpNode.DataShape.x,
+			aim_y : tmpNode.DataShape.y + BST.DATASHAPE_GAP_Y,
+		});
+		if(value < tmpNode.value)
 		{
-			Canvas.cmd
-			(
-				"Other", function(){
-					alert("很抱歉，我们没能在BST里找到 " + value + " 删除失败");
-				}
-			);
-			Canvas.cmd("FadeOut", deleteNode.DataShape);
-			break;
+			tmpMotherNode = tmpNode;
+			tmpNode = tmpNode.left;
 		}
+		else if(value > tmpNode.value)
+		{
+			tmpMotherNode = tmpNode;
+			tmpNode = tmpNode.right;
+		}
+		else		//找到了要删除的节点
+			break;
+	}
 
+	if(tmpNode == null)  //删除节点不存在
+	{
 		Canvas.cmd
 		(
-			"Move", deleteNode.DataShape,
-			{
-				aim_x : tmp.DataShape.x,
-				aim_y : tmp.DataShape.y + BST.DATASHAPE_GAP_Y,
+			"Other", function(){
+				alert("很抱歉，我们没能在BST里找到 " + value + " 删除失败");
 			}
 		);
-		
-		if(value < tmp.value)
-		{
-			mother = tmp;
-			tmp = tmp.left;
-		}
-		else if(value > tmp.value)
-		{
-			mother = tmp;
-			tmp = tmp.right;
-		}
-		else		//找到了
-		{
-			Canvas.cmd
-			(
-				"Move", deleteNode.DataShape,
-				{
-					aim_x : tmp.DataShape.x,
-					aim_y : tmp.DataShape.y,
-				}
-			);
-			Canvas.cmd("Delete", deleteNode.DataShape);
-			Canvas.cmd("FadeOut", tmp.DataShape);
+		Canvas.cmd("FadeOut", deleteValueShape);
+	}
+	else      //删除节点存在
+	{
+		Canvas.cmd("Move", deleteValueShape, {
+		 	aim_x : tmpNode.DataShape.x,
+			aim_y : tmpNode.DataShape.y,
+		});
+		Canvas.cmd("Delete", deleteValueShape);
 
-			if(tmp.left == null && tmp.right == null)  //删除的是叶子节点
+		if(tmpNode.left == null && tmpNode.right == null)  //删除的节点为叶子节点，直接删除该节点
+		{
+			swapNode = null;
+			deleteNode = tmpNode;
+			deleteMotherNode = tmpMotherNode;
+		}
+		else if(tmpNode.right == null)	//删除的节点只有左儿子,删除节点的值用左儿子值替换，然后删除左儿子
+		{
+			swapNode = tmpNode.left;
+			deleteNode = tmpNode.left;
+			deleteMotherNode = tmpNode;
+		}
+		else if(tmpNode.left == null)  //删除的节点只有右儿子，同上理
+		{
+			swapNode = tmpNode.right;
+			deleteNode = tmpNode.right;
+			deleteMotherNode = tmpNode;
+		}
+		else	//删除的节点有左右两个儿子，删除节点的值用左子树最大节点替换，然后删除左子树最大节点
+		{
+			var maxNode = tmpNode.left;  //查找删除节点左子树最大的节点
+			var maxMotherNode = tmpNode;
+			while(maxNode.right != null)
 			{
-				if(mother.left == tmp)
-				{
-					Canvas.cmd("FadeOut", mother.LeftPointerShape);
-					mother.left = null;
-				}
-				if(mother.right == tmp)
-				{
-					Canvas.cmd("FadeOut", mother.RightPointerShape);
-					mother.right = null;
-				}
+				maxMotherNode = maxNode;
+				maxNode = maxNode.right;
 			}
-			else if(tmp.left == null)   //删除的节点只有右儿子
-			{	
-				Canvas.cmd("StartParallel");
-				if(mother.left == tmp)
+		
+			swapNode = maxNode;
+			deleteNode = maxNode;
+			deleteMotherNode = maxMotherNode;
+		}
+		
+		if(swapNode != null)	//删除节点的值用swapNode节点的值替换
+		{
+			tmpNode.value = swapNode.value;
+			var swapValueShape = new Label({Canvas : Canvas, text : swapNode.value});
+			Canvas.cmd("Move", swapValueShape,{
+				x : swapNode.DataShape.x,
+				y : swapNode.DataShape.y,
+				aim_x : tmpNode.DataShape.x,
+				aim_y : tmpNode.DataShape.y,
+			});
+			Canvas.cmd("Draw", tmpNode.DataShape,{text : swapNode.value}, "Delete", swapValueShape);
+		}
+		if(deleteMotherNode != null)	//删除最终需要删除的节点(1.原删除节点2.原删除节点左儿子3.原删除节点右儿子4原删除节点左子树最大节点)
+			if(deleteMotherNode.left == deleteNode)
+				if(deleteNode.left != null)
 				{
-					Canvas.cmd("Move", mother.LeftPointerShape,
-					{
-						aimEndShape : tmp.right.DataShape,
-					});
-					mother.left = tmp.right;
+					deleteMotherNode.left = deleteNode.left;
+					Canvas.cmd("Move", delteMotherNode.LeftPointerShape,{aimEndShape : deleteNode.left.DataShape});
+					Canvas.cmd("FadeOut", deleteNode.DataShape, "FadeOut", deleteNode.LeftPointerShape);
 				}
-				if(mother.right == tmp)
+				else if(deleteNode.right != null)
 				{
-					Canvas.cmd("Move", mother.RightPointerShape,
-					{
-						aimEndShape : tmp.right.DataShape,
-					});
-					mother.right = tmp.right;
-				}
-				Canvas.cmd("FadeOut", tmp.RightPointerShape);
-				Canvas.cmd("EndParallel");
-			}
-			else if(tmp.right == null)	//删除的节点只有左儿子
-			{
-				Canvas.cmd("StartParallel");
-				if(mother.left == tmp)
-				{
-					Canvas.cmd("Move", mother.LeftPointerShape,
-					{
-						aimEndShape : tmp.left.DataShape,
-					});
-					mother.left = tmp.left;
-				}
-				if(mother.right == tmp)
-				{
-					Canvas.cmd("Move", mother.RightPointerShape,
-					{
-						aimEndShape : tmp.left.DataShape,
-					});
-					mother.right = tmp.left;
-				}
-				Canvas.cmd("FadeOut", tmp.LeftPointerShape);
-				Canvas.cmd("EndParallel");
-			}
-			else  //删除的节点有左右两个儿子
-			{
-				leftMaxNode = tmp.left;    //查找删除节点左子树最大的节点
-				leftMaxNodeMother = tmp;
-				while(leftMaxNode.right != null)  
-				{
-					leftMaxNodeMother = leftMaxNode;
-					leftMaxNode = leftMaxNode.right;
-				}
-				//交换删除节点和左子树最大节点的值
-				var swapvalue = tmp.value;
-				tmp.value = leftMaxNode.value;
-				leftMaxNode.value = swapvalue;
-				Canvas.cmd("StartParallel");
-				var tmpValueShape = new Label
-				Canvas.cmd("Move",)
-				//将左子树最大节点的父母指向null
-				if(leftMaxNodeMother.left == leftMaxNode)
-				{
-					Canvas.cmd("FadeOut",leftMaxNodeMother.LeftPointerShape);
-					leftMaxNodeMother.left = null;
+					deleteMotherNode.left = deleteNode.right;
+					Canvas.cmd("Move", delteMotherNode.LeftPointerShape,{aimEndShape : deleteNode.right.DataShape});
+					Canvas.cmd("FadeOut", deleteNode.DataShape, "FadeOut", deleteNode.RightPointerShape);
 				}
 				else
 				{
-					Canvas.cmd("FadeOut",RightMaxNodeMother.LeftPointerShape);
-					leftMaxNodeMother.Right = null;
+					deleteMotherNode.left = null;
+					Canvas.cmd("FadeOut", deleteNode.DataShape, "FadeOut", deleteMotherNode.LeftPointerShape);
 				}
-				//将删除节点的父母指向左子树最大节点
-				if(mother.left == tmp)
+			else
+				if(deleteNode.left != null)
 				{
-					Canvas.cmd("Move", mother.LeftPointerShape,
-					{
-						aimEndShape : leftMaxNode.DataShape,
-					});
-					mother.left = leftMaxNode;
+					deleteMotherNode.right = deleteNode.left;
+					Canvas.cmd("Move", delteMotherNode.RightPointerShape,{aimEndShape : deleteNode.left.DataShape});
+					Canvas.cmd("FadeOut", deleteNode.DataShape, "FadeOut", deleteNode.LeftPointerShape);
+				}
+				else if(deleteNode.right != null)
+				{
+					deleteMotherNode.right = deleteNode.right;
+					Canvas.cmd("Move", delteMotherNode.RightPointerShape,{aimEndShape : deleteNode.right.DataShape});
+					Canvas.cmd("FadeOut", deleteNode.DataShape, "FadeOut", deleteNode.RightPointerShape);
 				}
 				else
 				{
-					Canvas.cmd("Move", mother.RightPointerShape,
-					{
-						aimEndShape : leftMaxNode.DataShape,
-					});
-					mother.right = leftMaxNode;
+					deleteMotherNode.right = null;
+					Canvas.cmd("FadeOut", deleteNode.DataShape, "FadeOut", deleteMotherNode.RightPointerShape);
 				}
-				//将左子树最大节点的左右孩子指向删除节点的左右孩子
-				Canvas.cmd("Move",leftMaxNode.LeftPointerShape,{
-					aimEndShape : tmp.left.DataShape,
-				});
-				leftMaxNode.left = tmp.left;
-				Canvas.cmd("Move",leftMaxNode.RightPointerShape,{
-					aimEndShape : tmp.right.DataShape,
-				});
-				leftMaxNode.right = tmp.right;
-				
-				Canvas.cmd("FadeOut",tmp.LeftPointerShape);
-				Canvas.cmd("FadeOut",tmp.RightPointerShape);
-			}
-			break;
+		else	//删除的是根节点
+		{
+			this.root = null;
+			Canvas.cmd("FadeOut", deleteNode.DataShape);
 		}
 	}
+
 	Canvas.cmd("Other",function()
 	{
 		$(".controler").removeAttr("disabled");		//启用所有控制元素
@@ -376,65 +301,49 @@ BST.prototype.delete = function( value )
 BST.prototype.find = function( value )
 {
 	this.disableControlBar();
-
 	Canvas.cmd("Setup");
-	var findNode = new this.node( value );
-	Canvas.cmd
-	(
-		"Draw", findNode.DataShape,
-		{
-			x : BST.DATASHAPE_INSERT_X,
-			y : BST.DATASHAPE_INSERT_Y,
-		}
-	);
-	var tmp = this.root;
-	while(1)
+	var findValueShape = new Label({Canvas : Canvas, text : value});
+	Canvas.cmd("Draw", findValueShape, {
+		x : BST.DATASHAPE_INSERT_X,
+		y : BST.DATASHAPE_INSERT_Y,
+	});
+
+	var tmpNode = this.root;
+	while(tmpNode != null)   //查找节点
 	{
 		Canvas.cmd("Delay");
-		if(tmp == null)			//没有找到
-		{
-			Canvas.cmd
-			(
-				"Other", function(){
-					alert("很抱歉，我们没能在BST里找到 " + value);
-				}
-			);
-			Canvas.cmd("FadeOut", findNode.DataShape);
+		Canvas.cmd("Move", findValueShape, {
+			aim_x : tmpNode.DataShape.x,
+			aim_y : tmpNode.DataShape.y + BST.DATASHAPE_GAP_Y,
+		});
+		if(value < tmpNode.value)
+			tmpNode = tmpNode.left;
+		else if(value > tmpNode.value)
+			tmpNode = tmpNode.right;
+		else		//找到了节点
 			break;
-		}
-
-		Canvas.cmd
-		(
-			"Move", findNode.DataShape,
-			{
-				aim_x : tmp.DataShape.x,
-				aim_y : tmp.DataShape.y + BST.DATASHAPE_GAP_Y,
-			}
-		);
-		
-		if(value < tmp.value)
-			tmp = tmp.left;
-		else if(value > tmp.value)
-			tmp = tmp.right;
-		else		//找到了
-		{
-			Canvas.cmd("Other",function(){
-				alert("恭喜你，我们在BST里找到了 " + value);
-			});
-			Canvas.cmd
-			(
-				"Move", findNode.DataShape,
-				{
-					aim_x : tmp.DataShape.x,
-					aim_y : tmp.DataShape.y,
-				}
-			);
-			Canvas.cmd("Delete", findNode.DataShape);
-			break;
-		}
 	}
-	Canvas.cmd("Other",function()
+	
+	if(tmpNode == null)  //查找节点不存在
 	{
+		Canvas.cmd("Other", function() {
+				alert("很抱歉，我们没能在BST里找到 " + value + " 删除失败");
+		});
+		Canvas.cmd("FadeOut", findValueShape);
+	}
+	else      //查找节点存在
+	{
+		Canvas.cmd("Other",function(){
+			alert("恭喜你，我们在BST里找到了 " + value);
+		});
+		Canvas.cmd("Move", findValueShape, {
+		 	aim_x : tmpNode.DataShape.x,
+			aim_y : tmpNode.DataShape.y,
+		});
+		Canvas.cmd("Delete", findValueShape);
+	}
+
+	Canvas.cmd("Other",function() {
 		$(".controler").removeAttr("disabled");		//启用所有控制元素
 	});
 	Canvas.cmd("End");
