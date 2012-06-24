fe8f1a23e9fcb39282b129edd027b9bc67fb659d
KJlmfe <kjlmfe@gmail.com>
2012-06-24 15:01:52 +0800

    整理Queue(Array).js

--- Algorithm/Queue(Array).js
+++ Algorithm/Queue(Array).js
@@ -1,138 +1,161 @@
 function init()
 {
-	Canvas = new KJcanvas();  //用上面的canvas初始化一个全局画板对象(Canvas)
-	DataStructure = new Queue(); 	//初始化一个队列对象
+	Canvas = new KJcanvas();
+	DataStructure = new Queue(); 
 }
 
 Queue = function()
 {
+	Queue.ALGORITHM_NAME = "Queue(Array)"; 
+	Queue.SIZE = 7; 
+	Queue.OVERFLOW_INFO = "Queue is full,it will overflow if you enqueue anymore!";
+	Queue.EMPTY_INFO = "Queue is empty,it can't dequeue anymore!";
+	
+	Queue.FRAME_WIDTH = 60;
+	Queue.FRAME_HEIGHT = 60;
+	Queue.FRAME_X = Queue.FRAME_WIDTH/2 + 100;
+	Queue.FRAME_Y = Canvas.height / 2;
+	Queue.FRAME_BACKCOLOR = "FFF";
+	Queue.FRAME_EDGECOLOR = "000";
+	Queue.FRAME_TEXTCOLOR = "#F00";
+	Queue.FRAME_FONT = "18px sans-serif";
+	Queue.FRAME_EDGEWIDTH = 2;
+
+	Queue.SHAPE_WIDTH = 40;       
+	Queue.SHAPE_HEIGHT = 40;     
+	Queue.SHAPE_X = Queue.SHAPE_WIDTH/2 + 2;          
+	Queue.SHAPE_Y = Queue.SHAPE_HEIGHT/2 + 2;
+	Queue.SHAPE_BACKCOLOR = "#4D90FE"; 
+	Queue.SHAPE_EDGECOLOR = "000"; 
+	Queue.SHAPE_TEXTCOLOR = "FFF";
+	Queue.SHAPE_MOVESPEED = 5;  
+	Queue.SHAPE_FONT = "18px sans-serif";
+	Queue.SHAPE_EDGEWIDTH = 1;
+	
+	Queue.REAR_POINTER_X = Queue.FRAME_X;
+	Queue.REAR_POINTER_Y = Queue.FRAME_Y + Queue.FRAME_HEIGHT/2 + 100;
+	Queue.REAR_POINTER_FONT = "20px sans-serif";
+	Queue.REAR_POINTER_TEXTCOLOR = "#DAA520";
+	Queue.REAR_POINTER_MOVESPEED = 2;
+
+	Queue.FRONT_POINTER_X = Queue.FRAME_X;
+	Queue.FRONT_POINTER_Y = Queue.FRAME_Y - Queue.FRAME_HEIGHT/2 - 100;
+	Queue.FRONT_POINTER_FONT = "20px sans-serif";
+	Queue.FRONT_POINTER_TEXTCOLOR = "#32CD32";
+	Queue.FRONT_POINTER_MOVESPEED = 2;
+
 	this.addControls();
+	thisQueue = this;
 }
-Queue.ALGORITHM_NAME = "队列(数组)"; //算法名	
-Queue.SIZE = 7; //默认队列的大小
-Queue.OVERFLOW_INFO = "队列吃饱了,再压栈,队列会撑死的！";
-Queue.EMPTY_INFO = "队列里空空如也了,弹不出东西了！";
-
-Queue.FRAME_WIDTH = 60;
-Queue.FRAME_HEIGHT = 60;
-Queue.FRAME_START_X = 100;
-Queue.FRAME_START_Y = 250;//(Canvas.HEIGHT-Queue.FRAME_HEIGHT)/2;
-Queue.FRAME_TEXT = "";
-Queue.FRAME_BACKCOLOR = "FFF";
-Queue.FRAME_EDGECOLOR = "000";
-
-Queue.SHAPE_BACKCOLOR = "ABC";  //默认图形填充背景色
-Queue.SHAPE_EDGECOLOR = "000";  //默认图形边框颜色
-Queue.SHAPE_TEXTCOLOR = "C00";  //默认图形填充文本颜色
-Queue.SHAPE_TEXT = "shape";    //默认图新填充的文本内容
-Queue.SHAPE_WIDTH = 40;       //默认图形宽度
-Queue.SHAPE_HEIGHT = 40;      //默认图形高度
-Queue.SHAPE_START_X = 0;           //默认图新位置
-Queue.SHAPE_START_Y = 0;
-Queue.SHAPE_MOVE_SPEED = 5;  //默认图新移动速度
-Queue.SHAPE_MOVE_PATH = "LINE"; //默认图新移动方式(直线)
-Queue.SHAPE_FONT = "10px sans-serif";
-
-Queue.REAR_POINTER_FONT = "20px sans-serif";
-Queue.REAR_POINTER_MOVE_SPEED = 2;
-Queue.REAR_POINTER_START_X = Queue.FRAME_START_X;
-Queue.REAR_POINTER_START_Y = Queue.FRAME_START_Y + (Queue.FRAME_HEIGHT+100);
-Queue.REAR_POINTER_COLOR = "000";
-
-Queue.FRONT_POINTER_FONT = "20px sans-serif";
-Queue.FRONT_POINTER_MOVE_SPEED = 2;
-Queue.FRONT_POINTER_START_X = Queue.FRAME_START_X;
-Queue.FRONT_POINTER_START_Y = Queue.FRAME_START_Y - (Queue.FRAME_HEIGHT+100);
-Queue.FRONT_POINTER_COLOR = "000";
-
 Queue.prototype = new Algorithm();
-Queue.prototype.create = function(queueSize)  //初始化队列大小,并绘制该队列
+Queue.prototype.create = function(queueSize) 
 {
-	this.queue = new Array();  //队列 
+	this.disableControlBar();
+	this.queue = new Array(); 
 	this.rear = 0;
 	this.front = 0;
 	this.size = Queue.SIZE;
 	if(Positive_Integer.test(queueSize))
 		this.size = queueSize;
-
-	this.frame = new Array();  //数组框架
-	this.front_pointer = new Label({
+	this.frame = new Array();  
+	this.front_pointer = new Label
+	({
 		Canvas : Canvas,
-		text : "front="+this.front,
-		textColor : Queue.FRONT_POINTER_COLOR,
+		text : "front=" + this.front,
+		x : Queue.FRONT_POINTER_X,
+		y : Queue.FRONT_POINTER_Y,
 		font : Queue.FRONT_POINTER_FONT,
-		moveSpeed : Queue.FRONT_POINTER_MOVE_SPEED});
-	this.rear_pointer = new Label({
+		textColor : Queue.FRONT_POINTER_TEXTCOLOR,
+		moveSpeed : Queue.FRONT_POINTER_MOVESPEED
+	});
+	this.rear_pointer = new Label
+	({
 		Canvas : Canvas,
-		text : "rear="+this.rear,
-		textColor : Queue.REAR_POINTER_COLOR,
+		text : "rear=" + this.rear,
+	    x : Queue.REAR_POINTER_X,
+		y : Queue.REAR_POINTER_Y,
 		font : Queue.REAR_POINTER_FONT,
-		moveSpeed : Queue.REAR_POINTER_MOVE_SPEED});
+		textColor : Queue.REAR_POINTER_TEXTCOLOR,
+		moveSpeed : Queue.REAR_POINTER_MOVESPEED
+	});
 	
 	Canvas.init();
 	Canvas.cmd("Setup");
 	Canvas.cmd("StartParallel");
-	Canvas.cmd("Draw",this.front_pointer,{
-		x : Queue.FRONT_POINTER_START_X,
-		y : Queue.FRONT_POINTER_START_Y});
-	Canvas.cmd("Draw",this.rear_pointer,{
-	    x : Queue.REAR_POINTER_START_X,
-		y : Queue.REAR_POINTER_START_Y});
+	Canvas.cmd
+	(
+		"Draw",this.front_pointer,
+		"Draw",this.rear_pointer
+	);
 	for(i=0; i<this.size; i++)
 	{
-		this.frame[i] = new Rectangle({
+		this.frame[i] = new Rectangle
+		({
 			Canvas : Canvas,
+			text : i, 
 			width : Queue.FRAME_WIDTH, 
 			height : Queue.FRAME_HEIGHT, 
-			text : i, 
+			x : Queue.FRAME_X + i*Queue.FRAME_WIDTH, 
+			y : Queue.FRAME_Y,
 			backColor : Queue.FRAME_BACKCOLOR, 
-			edgeColor : Queue.FRAME_EDGECOLOR
-			}); 
-		Canvas.cmd("Draw",this.frame[i],{
-			x : Queue.FRAME_START_X+i*Queue.FRAME_WIDTH, 
-			y : Queue.FRAME_START_Y});
+			edgeColor : Queue.FRAME_EDGECOLOR,
+			edgeColor : Queue.FRAME_TEXTCOLOR,
+			font : Queue.FRAME_FONT = "18px sans-serif",
+			edgeWidth : Queue.FRAME_EDGEWIDTH
+		}); 
+		Canvas.cmd("Draw", this.frame[i]);
 	}
 	Canvas.cmd("EndParallel");
+	Canvas.cmd("Other", function()
+	{
+		thisQueue.enableControlBar();
+	});
 	Canvas.cmd("End");
 }
-Queue.prototype.enqueue = function( value )
+Queue.prototype.enqueue = function(value)
 {
 	if(this.rear >= this.size)
 		alert(Queue.OVERFLOW_INFO);
 	else
 	{
-		this.disableControlBar();
-	
-		this.queue[this.rear] = new Rectangle({
+		this.disableControlBar();	
+		this.queue[this.rear] = new Rectangle
+		({
 			Canvas : Canvas,
+			text : value,
 			width : Queue.SHAPE_WIDTH, 
 			height : Queue.SHAPE_HEIGHT,
-			text : value,
+			x : Queue.SHAPE_X,
+			y : Queue.SHAPE_Y,
 			backColor : Queue.SHAPE_BACKCOLOR, 
 			edegeColor : Queue.SHAPE_EDGECOLOR,
 			textColor : Queue.SHAPE_TEXTCOLOR,
-			font : Queue.SHAPE_FONT
-			});
+			moveSpeed : Queue.SHAPE_MOVESPEED,
+			font : Queue.SHAPE_FONT,
+			edgeWidth : Queue.SHAPE_EDGEWIDTH,
+		});
 
 		Canvas.cmd("Setup");	
-		Canvas.cmd("Draw",this.queue[this.rear],{
-			x : Queue.SHAPE_START_X,
-			y : Queue.SHAPE_START_Y});
- 		Canvas.cmd("Delay",Canvas.DELAY_TIME);
- 		Canvas.cmd(
-			"Move", this.queue[this.rear],{
-			aim_x : this.frame[this.rear].x,
-			aim_y : this.frame[this.rear].y,
-			moveSpeed : Queue.SHAPE_MOVE_SPEED
-			},
-			
-			"Move",this.rear_pointer,{
-			text : "rear=" + (this.rear+1),
-			aim_x : this.rear_pointer.x + Queue.FRAME_WIDTH,
-			moveSpeed : Queue.REAR_POINTER_MOVE_SPEED
-			});
-		Canvas.cmd("Other",function(){
-			$(".controler").removeAttr("disabled");		//启用所有控制元素
+		Canvas.cmd("FadeIn", this.queue[this.rear])
+ 		Canvas.cmd
+		(
+			"Move", this.queue[this.rear],
+			{
+				aim_x : this.frame[this.rear].x,
+				aim_y : this.frame[this.rear].y,
+			}
+		);
+		Canvas.cmd
+		(
+			"Move", this.rear_pointer,
+			{
+				text : "rear=" + (this.rear+1),
+				aim_x : this.rear_pointer.x + Queue.FRAME_WIDTH,
+			}
+		);
+		Canvas.cmd("Other", function()
+		{
+			thisQueue.enableControlBar();
 		});
 		Canvas.cmd("End");
 		this.rear++;
@@ -146,34 +169,41 @@ Queue.prototype.dequeue = function()
 	{
 		this.disableControlBar();
 		Canvas.cmd("Setup");
-	   	Canvas.cmd(
+	   	Canvas.cmd
+		(
 			"Move",this.queue[this.front],
 			{
-				aim_x : Queue.SHAPE_START_X,
-				aim_y : Queue.SHAPE_START_Y,
-			},
-			"Move",this.front_pointer,
-			{
-				text : "front=" + (this.front+1),
-				aim_x : this.frame[this.front].x+Queue.FRAME_WIDTH,
+				aim_x : Queue.SHAPE_X,
+				aim_y : Queue.SHAPE_Y,
 			}
 		);
-		Canvas.cmd("Delay",Canvas.DELAY_TIME);
-	
-		Canvas.cmd("Delete",this.queue[this.front]);
-		Canvas.cmd("Other",function(){
-			$(".controler").removeAttr("disabled");		//启用所有控制元素
+		Canvas.cmd
+		(
+			"Move",this.front_pointer,
+			{
+				text : "front=" + (this.front + 1),
+				aim_x : this.frame[this.front].x + Queue.FRAME_WIDTH,
+			}
+		)
+		Canvas.cmd("FadeOut", this.queue[this.front]);
+		Canvas.cmd("Other", function()
+		{
+			thisQueue.enableControlBar();
 		});
 		Canvas.cmd("End");
-	 	this.front++;
+ 		this.front++;
 	}
 }
 Queue.prototype.addControls = function()
 {
 	var obj = this;
 	$("#AlgorithmName").html(Queue.ALGORITHM_NAME);
+	
 	this.TextInput = this.addControlBar("text","");
 	this.CreatQueueButton = this.addControlBar("button","Creat Queue");
+	this.EnqueueButton = this.addControlBar("button","Enqueue");
+	this.DequeueButton = this.addControlBar("button","Dequeue");
+
 	this.CreatQueueButton.onclick = function(){
 		var queueSize = obj.TextInput.value;
 		obj.create(queueSize);
@@ -181,14 +211,12 @@ Queue.prototype.addControls = function()
 		obj.EnqueueButton.disabled = false;
 	}
 
-	this.EnqueueButton = this.addControlBar("button","Enqueue");
 	this.EnqueueButton.onclick = function(){
 		var value = obj.TextInput.value;
 		obj.enqueue(value);	
 		obj.TextInput.value = "";
 	}
 
-	this.DequeueButton = this.addControlBar("button","Dequeue");
 	this.DequeueButton.onclick = function(){
 		obj.dequeue();
 	}
